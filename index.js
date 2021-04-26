337ebe6366444c1d95268b8f7e855720d7f725c4
Amro dandashli <amrdandashli@gmail.com>
2021-04-26 02:22:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData("Name","email") {
+
+  const formData = {
+    Name: "Amro",
+    email: "amrdandashli@gmail.com"
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    });
+    .catch(function(error) {
+   alert("Bad things! Ragnarők!");
+   console.log(error.message);
+ });
+ return fetch()
+}

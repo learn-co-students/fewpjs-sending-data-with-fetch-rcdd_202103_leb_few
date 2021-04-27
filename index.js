143556dfccf0f5f14cdf0287c74abb87669d6554
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-04-27 12:06:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(n,e){
+  return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: n,
+    email: e
+  })
+})
+ .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+     document.body.innerHTML = object[ "id" ] 
+    console.log(object);
+  })
+  .catch(function(error) {
+    document.body.innerHTML = error.message 
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });
+}

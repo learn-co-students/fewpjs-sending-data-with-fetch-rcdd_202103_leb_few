ccc8ea1c6dd3774c1d6ad246893a271284dfef3e
Nour baltaji <nourbaltaji99@gmail.com>
2021-05-31 08:53:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+function submitData(name, email){
+
+    const formData = {
+    userName: name,
+    userEmail: email
+  };
+
+  // const configObj = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Accept": "application/json"
+  //   },
+  //   body: JSON.stringify(formData)
+  // };
+
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  }
+  return `${fetch('http://localhost:3000/users', configObj)
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(object) {
+            console.log(object.id);
+          })
+          .catch((error)=>{
+            alert("Bad things! fetch failed!");
+              console.log(error.message);
+          })}`;
+
+
+}
+submitData('rex ', 'ee');
